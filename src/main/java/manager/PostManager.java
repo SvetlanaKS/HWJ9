07d225088036eea4299f5bf2e20abc027d20a35e
Svet21 <chuyak.svetlana@gmail.com>
2021-08-24 14:10:27 +0500

    Add new class

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,12 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    public Post[] search(int ownerId, String domain, String query, boolean ownerOnly, int count, int offset) {
+        return null;
+    }
+    public void delete(int ownerId, int postId) {
+    }
+    
+}
